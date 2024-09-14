32a70c9201f91babc97445f439c2d9ee0fa567d0
Poonam-CBS <82645216+Poonam-CBS@users.noreply.github.com>
2024-09-14 14:28:28 +0200

    Create guess_the_game.py

---
+++ guess_the_game.py
@@ -0,0 +1,23 @@
+def guess_the_number():
+    selected_num = random.randint(1,100) 
+    #return selected_num: This was a check in the beginning 
+    while True:
+        user_guess = input("Guess the number; it must be an integer: ")
+        if '.' in user_guess:
+            print("Invalid guess, please guess an integer")
+            continue
+        if int(user_guess) > int(selected_num):
+            print("Too high")
+        elif int(user_guess) < int(selected_num):
+            print("Too low!!") 
+        else: #user_guess = selected_num 
+            print("Congratulations! you guessed it right!!")
+            return
+    return
+
+begin=input("start the play? (y/n): ")
+if begin=="n":
+    print("See you next time!, have a good day!")
+elif begin=="y":
+    guess_the_number()
+else: print("Invalid response!, please select either 'y' for yes or 'n' for no")
