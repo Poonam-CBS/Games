d1dffc479ed904dd647ca35515e84a8164200580
Poonam-CBS <82645216+Poonam-CBS@users.noreply.github.com>
2024-09-15 17:14:39 +0200

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -5,14 +5,14 @@ def guess_the_number():
     #return selected_num: This was a check in the beginning 
     while True:
         user_guess = input("Guess the number; it must be an integer: ")
-        if '.' in user_guess or re.search("[a-z]",user_guess):
+        if '.' in user_guess or re.search("[a-z]",user_guess): #This is to avoid real number and alphabets
             print("Invalid guess, please guess an integer")
             continue
-        if int(user_guess) > int(selected_num):
+        if int(user_guess) > int(selected_num): #If the guessed number is higher than the selected number
             print("Too high")
-        elif int(user_guess) < int(selected_num):
+        elif int(user_guess) < int(selected_num): #If the guesses number is lower than the selected number
             print("Too low!!") 
-        else: #user_guess = selected_num 
+        else: #user_guess = selected_num #If the guessed number is equal to the seceted number
             print("Congratulations! you guessed it right!!")
             return
     return
