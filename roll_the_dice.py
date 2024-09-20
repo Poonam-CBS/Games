1bb78c4d8ac3a16f597afd07719a13001d018a3d
Poonam-CBS <82645216+Poonam-CBS@users.noreply.github.com>
2024-09-20 16:24:46 +0200

    Update roll_the_dice.py

--- roll_the_dice.py
+++ roll_the_dice.py
@@ -1,8 +1,8 @@
 import random
-def roll_the_dice():
+def rolling_a_dice():
     #Do the loop while it is true
     while True:
-         dice_roll = random.randint(1,6) # chose a number between 1-6 randomly
+         dice_roll = random.randint(1,6) # the computer will chose a number between 1-6 randomly
          print(f"you rolled {dice_roll}")
          replay=input("do you want to play again? (y/n): ") 
          if replay == "y":
@@ -20,5 +20,5 @@ begin=input("start the play? (y/n): ")
 if begin=="n":
     print("See you next time!, have a good day!")
 elif begin=="y":
-    roll_the_dice()
+    rolling_a_dice()
 else: print("Invalid response!, please select either 'y' for yes or 'n' for no")
